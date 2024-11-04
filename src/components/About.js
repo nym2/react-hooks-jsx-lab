1bef8f5b909e6259e8859c911e34794e5753624b
NymphaPamba <praisepamba@gmail.com>
2024-11-05 02:40:20 +0300

    initial commits

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+    <h2>About Me</h2>
+    <p>The number of my scars are the number of my victory, that's why my praise can never be the same as yours</p>
+    <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
